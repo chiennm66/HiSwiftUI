3d5cbcdeba5837d456de8388dad5723aaab1c591
chienabc <chien97666@gmail.com>
2025-05-31 18:35:31 +0700

    Initial Commit

---
+++ HiSwiftUI/HiSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiSwiftUIApp.swift
+//  HiSwiftUI
+//
+//  Created by CHIEN on 31/5/25.
+//
+
+import SwiftUI
+
+@main
+struct HiSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
