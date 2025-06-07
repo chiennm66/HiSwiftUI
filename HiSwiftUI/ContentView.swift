5d2ee70f57d3027f8cc9744baf473a25a824e94d
chienabc <chien97666@gmail.com>
2025-06-07 18:56:16 +0700

    Lesson 7

--- HiSwiftUI/ContentView.swift
+++ HiSwiftUI/ContentView.swift
@@ -8,15 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var message = "XIN CHÀO CÁC BẠN"
+    @State private var imageName = "deskview"
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.blue)
+                .frame(width: 300, height: 300)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+                .foregroundStyle(.red)
+            // màu, font chữ, size hình ảnh
+            // ADD - BUTTON
+            
+            HStack {
+                Button("CLICK ME 1"){
+                    print("change text1")
+                    message = "HÔM NAY LÀ T7"
+                    imageName = "pencil"
+                }
+                Button("CLICK ME 2"){
+                    print("change text2")
+                    message = "HÔM NAY LÀ CN"
+                    imageName = "trash"
+                }
+            }
+            .font(.title3)
+            .tint(.red)
+            .buttonStyle(.borderedProminent)
+
+            
         }
+        
+        //UI
+        
+        
         .padding()
     }
+    
+    
 }
 
 #Preview {
